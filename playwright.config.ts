9e306d79433ea332ba0b580ee85dc8783e91484f
eldariko <eldariko15@gmail.com>
2026-02-25 17:37:40 +0200

    reorder and update browser projects in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -56,12 +56,12 @@ const config: PlaywrightTestConfig = {
 
     /* Configure projects for major browsers */
     projects: [
-        {
-            name: 'chromium',
-            use: {
-                ...devices['Desktop Chrome'],
-            },
-        },
+        // {
+        //     name: 'chromium',
+        //     use: {
+        //         ...devices['Desktop Chrome'],
+        //     },
+        // },
 
         // {
         //   name: 'firefox',
@@ -70,12 +70,12 @@ const config: PlaywrightTestConfig = {
         //   },
         // },
 
-        // {
-        //   name: 'webkit',
-        //   use: {
-        //     ...devices['Desktop Safari'],
-        //   },
-        // },
+        {
+          name: 'webkit',
+          use: {
+            ...devices['Desktop Safari'],
+          },
+        },
 
         /* Test against mobile viewports. */
         // {
